493a3d7cac2021e643aa270a94277011ec306851
apeckyz <apeckyz@gmail.com>
2026-01-31 22:40:28 +0000

    favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,31 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: 'Premium Service Business Template',
   description: 'A designer-led, premium landing page template for service businesses',
+  icons: {
+    icon: [
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [{ url: '/favicon/apple-touch-icon.png', sizes: '180x180' }],
+    shortcut: ['/favicon/favicon.ico'],
+  },
+  manifest: '/favicon/site.webmanifest',
+  openGraph: {
+    title: 'Premium Service Business Template',
+    description: 'A designer-led, premium landing page template for service businesses',
+    type: 'website',
+    images: [
+      {
+        url: '/tr1.jpg',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Premium Service Business Template',
+    description: 'A designer-led, premium landing page template for service businesses',
+    images: ['/tr1.jpg'],
+  },
 }
 
 export default function RootLayout({
